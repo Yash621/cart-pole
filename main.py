7f6264a621490bfdc29056728754c880d44b5dfd
yash621 <yg17381@gmail.com>
2022-01-02 21:36:28 +0530

    project completed

--- main.py
+++ main.py
@@ -10,11 +10,6 @@ from collections import deque
 env = gym.make('CartPole-v0')
 print(env.observation_space)
 print(env.action_space)
-# while True:
-#     env.reset()
-#     env.render()
-#     action = env.action_space.sample()
-#     env.step(action)
 
 
 class Policy(nn.Module):
@@ -41,5 +36,52 @@ optimizer = optim.Adam(policy.parameters(), lr=0.01)
 
 
 def reinforce(n_episodes=1000, max_t=1000, gamma=1.0, print_every=100):
-    sources_deque = deque(maxlen=100)
+    scores_deque = deque(maxlen=100)
     scores = []
+    for i_episode in range(1, n_episodes + 1):
+        saved_log_probs = []
+        rewards = []
+        state = env.reset()
+        for t in range(1, max_t+1):
+            action, log_prob = policy.act(state)
+            saved_log_probs.append(log_prob)
+            state, reward, done, _ = env.step(action)
+            rewards.append(reward)
+            env.render()
+            if done:
+                break
+        scores.append(sum(rewards))
+        scores.append(sum(rewards))
+        discount = [gamma**i for i in range(len(rewards))]
+        R = sum([a * b for a, b in zip(discount, rewards)])
+        policy_loss = []
+        for log_prob in saved_log_probs:
+            policy_loss.append(-log_prob * R)
+        policy_loss = torch.cat(policy_loss).sum()
+        optimizer.zero_grad()
+        policy_loss.backward()
+        optimizer.step()
+        if i_episode % print_every == 0:
+            print('Episode {}\tAverage Score: {:.2f}'.format(
+                i_episode-100, np.mean(scores_deque)))
+        avg_score = np.mean(scores_deque)
+        if avg_score >= 195.0:
+            print('Environment solved in {} episodes!'.format(i_episode-100))
+            break
+
+        return scores
+
+
+scores = reinforce()
+env = gym.make('CartPole-v0')
+
+for _ in range(100):
+    state = env.reset()
+    while True:
+        action, _ = policy.act(state)
+        env.render()
+        state, reward, done, _ = env.step(action)
+        if done:
+            break
+
+env.close()
